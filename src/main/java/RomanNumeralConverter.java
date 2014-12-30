7763112bf1509ed9c45117e0bca6a5607095bc7f
Brandon Cromer <bcromer@pillartechnology.com>
2014-12-30 13:13:10 -0500

    Mike wins

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -6,6 +6,7 @@ import java.util.TreeMap;
 public class RomanNumeralConverter {
 
     private static final TreeMap<Integer, String> map;
+    private static final TreeMap<Integer, String> cheatersMap;
 
     static {
         map = new TreeMap<>();
@@ -16,6 +17,14 @@ public class RomanNumeralConverter {
         map.put(10, "X");
         map.put(5, "V");
         map.put(1, "I");
+
+        cheatersMap = new TreeMap<>();
+        map.put(4, "IV");
+        map.put(9, "IX");
+        map.put(40, "XL");
+        map.put(90, "XC");
+        map.put(400, "CD");
+        map.put(900, "CM");
     }
 
     public static String convertArabicToRoman(int arabic) {
@@ -27,6 +36,10 @@ public class RomanNumeralConverter {
 
             for (int i = 0; i < multipleOfKey; i++) {
                 sb.append(map.get(key));
+
+                if (multipleOfKey > 3) {
+                    sb.replace(0, sb.length(), cheatersMap.get(arabic));
+                }
             }
 
             currentValue[0] %= key;
--- src/test/groovy/RomanNumeralConverterSpec.groovy
+++ src/test/groovy/RomanNumeralConverterSpec.groovy
@@ -11,15 +11,23 @@ class RomanNumeralConverterSpec extends Specification {
         where:
         expected | input
         "I"      | 1
-//        "II"     | 2
-//        "III"    | 3
+        "II"     | 2
+        "III"    | 3
+        "IV"     | 4
         "V"      | 5
+        "IX"     | 9
         "X"      | 10
         "XV"     | 15
         "L"      | 50
+        "LXXV"   | 75
+        "LXXXV"  | 85
+        "XCIX"   | 99
         "C"      | 100
         "D"      | 500
+        "CMXCIX" | 999
         "M"      | 1000
+        "MMM"    | 3000
+        "MMMDCCCXCIX" | 3899
     }
 
 }
